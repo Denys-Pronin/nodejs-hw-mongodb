f22017cc43192184aba51cf418dde1da376e7356
Denys Pronin <denispronindev@gmail.com>
2024-09-20 14:06:19 +0300

    Finish

--- src/server.js
+++ src/server.js
@@ -2,20 +2,18 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+
+import contactsRouter from './routes/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
-  //1) Створення серверу за допомогою виклику express()
   const app = express();
 
   app.use(express.json());
-
-  //2) Налаштування cors
   app.use(cors());
-
-  //2) Налаштування логгера pino
   app.use(
     pino({
       transport: {
@@ -24,41 +22,10 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successgully found contacts!',
-      data: contacts,
-    });
-  });
+  app.use(contactsRouter);
+  app.use(errorHandler);
+  app.use(notFoundHandler);
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact no found',
-      });
-      return;
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}`,
-      data: contact,
-    });
-  });
-
-  //3) Обробка неіснуючих роутів
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  //4) Запуск серверу
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
