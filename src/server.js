4e49b0e58bb6dc12bebe41e7ba7b267100790a56
Denys Pronin <denispronindev@gmail.com>
2024-10-15 16:38:59 +0300

    Finish hw

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { UPLOAD_DIR } from './constants/index.js';
 import router from './routes/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import cookieParser from 'cookie-parser';
 
 const PORT = Number(env('PORT', '3000'));
@@ -27,6 +28,7 @@ export const setupServer = () => {
 
   app.use(router);
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
